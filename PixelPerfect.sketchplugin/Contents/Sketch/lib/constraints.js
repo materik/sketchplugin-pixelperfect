dd124983f76e7e97004ef4e308f831fae29c4faa
Mattias Eriksson <thematerik@gmail.com>
2018-05-03 09:39:05 -0700

    [constraints] add a component method

--- PixelPerfect.sketchplugin/Contents/Sketch/lib/constraints.js
+++ PixelPerfect.sketchplugin/Contents/Sketch/lib/constraints.js
@@ -11,6 +11,10 @@ Constraints.new = function(layer) {
 
 // Getter
 
+Constraints.prototype.component = function() {
+    return Component.new(this._layer)
+}
+
 Constraints.prototype.hasFixedWidth = function() {
     return this._layer.hasFixedWidth()
 }
@@ -105,7 +109,7 @@ Constraints.prototype.apply = function(properties) {
     this.setHasFixedWidth(!(this.hasFixedRight() && this.hasFixedLeft()));
     this.setHasFixedHeight(!(this.hasFixedTop() && this.hasFixedBottom()));
 
-    Component.new(this._layer).debug("^ Constraints: apply: <" + this._layer.name() + "> " + this.toString(), 1)
+    this.component().debug("^ Constraints: apply:" + this.toString(), 1)
 }
 
 Constraints.prototype.reset = function() {
@@ -113,7 +117,7 @@ Constraints.prototype.reset = function() {
 }
 
 Constraints.prototype.lock = function() {
-    Component.new(this._layer).debug("^ Constraints: lock <" + this._layer.name() + ">", 1)
+    this.component().debug("^ Constraints: lock>", 1)
 
     this._lockedHasFixedWidth = this.hasFixedWidth()
     this._lockedHasFixedHeight = this.hasFixedHeight()
@@ -130,7 +134,7 @@ Constraints.prototype.lock = function() {
 }
 
 Constraints.prototype.unlock = function() {
-    Component.new(this._layer).debug("^ Constraints: unlock <" + this._layer.name() + ">", 1)
+    this.component().debug("^ Constraints: unlock", 1)
 
     this.reset()
     this.setHasFixedWidth(this._lockedHasFixedWidth)
