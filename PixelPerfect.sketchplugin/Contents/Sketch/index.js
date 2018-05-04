8066d79fabf0c7b3d999241e8ec00a8eb6d2fd46
Mattias Eriksson <thematerik@gmail.com>
2018-05-04 05:27:28 -0700

    [import] update the imports for both test and sketch to include all files

--- PixelPerfect.sketchplugin/Contents/Sketch/index.js
+++ PixelPerfect.sketchplugin/Contents/Sketch/index.js
@@ -11,8 +11,16 @@ var global = {};
 @import "./lib/properties.js";
 @import "./lib/property.js";
 @import "./lib/utils.js";
+
 @import "./lib/components/artboard.js";
 @import "./lib/components/group.js";
+@import "./lib/components/shape.js";
 @import "./lib/components/symbol-instance.js";
 @import "./lib/components/symbol-master.js";
 @import "./lib/components/text.js";
+
+@import "./lib/properties/center.js";
+@import "./lib/properties/margin.js";
+@import "./lib/properties/padding.js";
+@import "./lib/properties/size.js";
+@import "./lib/properties/stack.js";
--- test/lib/index.js
+++ test/lib/index.js
@@ -1,20 +1,28 @@
 
 global.assert = require('assert');
 
-require('../../PixelPerfect.sketchplugin/Contents/Sketch/settings.js');
-require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/component.js');
-require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/components.js');
-require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/constraints.js');
-require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/frame.js');
-require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/index.js');
-require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/padding.js');
-require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/properties.js');
-require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/property.js');
-require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/utils.js');
-require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/components/artboard.js');
-require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/components/group.js');
-require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/components/symbol-instance.js');
-require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/components/symbol-master.js');
-require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/components/text.js');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/settings');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/component');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/components');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/constraints');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/frame');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/index');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/padding');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/properties');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/property');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/utils');
+
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/components/artboard');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/components/group');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/components/shape');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/components/symbol-instance');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/components/symbol-master');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/components/text');
+
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/properties/center');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/properties/margin');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/properties/padding');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/properties/size');
+require('../../PixelPerfect.sketchplugin/Contents/Sketch/lib/properties/stack');
 
 require('./mock');
