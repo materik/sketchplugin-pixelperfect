c46cf041edb1006a40b4ca1d04d27e2c24151ec8
Mattias Eriksson <thematerik@gmail.com>
2018-05-19 02:33:22 -0700

    [scripts] remove some unused scripts

--- scripts/update-metadata.js
+++ scripts/update-metadata.js
@@ -3,26 +3,14 @@
 var fs = require('fs');
 var json = require('edit-json-file');
 
-var MANIFEST_FILE = './PixelPerfect.sketchplugin/Contents/Sketch/manifest.json';
 var PACKAGE_FILE = './package.json';
-var PACKAGE_LOCK_FILE = './package-lock.json';
 var README_FILE = './README.md';
 
 // Get data from package.json
 
 var pkg = json(PACKAGE_FILE);
-var name = pkg.get('name');
 var description = pkg.get('description');
 var version = pkg.get('version');
-
-// Update manifest.json
-
-var manifest = json(MANIFEST_FILE, {
-    stringify_width: 4
-});
-manifest.set('description', description);
-manifest.set('version', version);
-manifest.save();
 
 // Update README.md
 
